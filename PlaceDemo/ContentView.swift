8ee9a74c9e42d6b9ad8176b021cdcc446fb6df34
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2022-03-16 15:32:41 +0900

    Initial Commit

---
+++ PlaceDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlaceDemo
+//
+//  Created by 斉藤 祐輔 on 2022/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlaceDemo/PlaceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceDemoApp.swift
+//  PlaceDemo
+//
+//  Created by 斉藤 祐輔 on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct PlaceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
